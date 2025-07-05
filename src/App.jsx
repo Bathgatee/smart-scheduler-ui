caf4681c0feaa711ea144c1e3c72027462887c98
Bathgatee <jayjasonjay@yahoo.com>
2025-07-04 20:33:55 -0400

    Vercel deployment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import LicenseDownload from "./components/LicenseDownload";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
+      <LicenseDownload />
+    </div>
+  );
+}
+
+export default App;
